b01de0c2bf062b21c00cf4331dc52b736beac5f3
Chyrion <laine.vili@gmail.com>
2023-04-12 14:40:21 +0300

    db.py fixed to work uniformly across environments

--- db.py
+++ db.py
@@ -4,10 +4,6 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
-# This top option is for deploying to fly
-# In development it breaks the thing that it fixes in production
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv(
     'DATABASE_URL').replace('://', 'ql://', 1)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 db = SQLAlchemy(app)
